17ad6954f59a116fd46ad85c523673bcd75e804f
Daniel Johnson <danjohnson77@gmail.com>
2021-10-22 12:29:41 -0700

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import { useEffect, useState } from "react";
+import "../styles/globals.scss";
+import Layout from "../components/Layout";
+import Loading from "../components/Loading";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }) {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = () => {
+      setLoading(true);
+    };
+    const handleComplete = () => {
+      setLoading(false);
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router]);
+
+  return <Layout>{loading ? <Loading /> : <Component {...pageProps} />}</Layout>;
 }
 
-export default MyApp
+export default MyApp;
